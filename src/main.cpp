0458f43e6f082471d6bdaed036a93c053ee39742
Thomas Dooms <43652322+funkymaniac@users.noreply.github.com>
2020-03-01 18:42:49 +0100

    cool stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,85 +2,146 @@
 #include <filesystem>
 #include <antlr4-runtime.h>
 
-#include "JSONLexer.h"
-#include "JSONParser.h"
-#include "JSONVisitor.h"
+#include <CLexer.h>
+#include <CParser.h>
+#include <CVisitor.h>
 
-struct DotVisitor : public JSONVisitor
+
+struct DotVisitor : public CVisitor
 {
-    explicit DotVisitor(std::filesystem::path input) : path(std::move(input))
+    explicit DotVisitor(std::filesystem::path path) : path(std::move(path))
     {
-        file = std::ofstream(path.replace_extension("dot"));
-        if(not file.is_open())
-            throw std::runtime_error("could not open file with path: " + path.string());
+        std::filesystem::create_directory(this->path.parent_path());
+        stream = std::ofstream(this->path.string() + ".dot");
+        if(not stream.is_open()) throw std::runtime_error("could not open file: " + this->path.string() + ".dot");
 
-        file << "digraph G{\n";
+        stream << "digraph G\n";
+        stream << "{\n";
     }
     ~DotVisitor() final
     {
-        const auto dot = path.replace_extension("dot");
-        const auto png = path.replace_extension("png");
+        stream << "\"0\"[style = invis];\n";
+        stream << "}\n" << std::flush;
+        stream.close();
 
-        file << "}" << std::flush;
-        std::cerr << system(("dot -Tpng " + dot.string() + " -o " + png.string()).c_str()) << '\n';
-    }
+        const auto dot = path.string() + ".dot";
+        const auto png = path.string() + ".png";
 
-    antlrcpp::Any visitJson(JSONParser::JsonContext* context) override
-    {
-        writeToFile("json: ", context);
-        return visitChildren(context);
-    }
-    antlrcpp::Any visitObject(JSONParser::ObjectContext* context) override
-    {
-        writeToFile("object: ",context);
-        return visitChildren(context);
-    }
-    antlrcpp::Any visitPair(JSONParser::PairContext* context) override
-    {
-        writeToFile("pair: ",context);
-        return visitChildren(context);
-    }
-    antlrcpp::Any visitArray(JSONParser::ArrayContext* context) override
-    {
-        writeToFile("array: ",context);
-        return visitChildren(context);
-    }
-    antlrcpp::Any visitValue(JSONParser::ValueContext* context) override
-    {
-        writeToFile("value: ", context);
-        return visitChildren(context);
+        system(("dot -Tpng " + dot + " -o " + png).c_str());
+        std::filesystem::remove(dot);
     }
 
-    void writeToFile(const std::string& name, antlr4::ParserRuleContext* context)
+    antlrcpp::Any visitBasicExpression(CParser::BasicExpressionContext* context) override
     {
-        if(context->parent != nullptr)
-        {
-            file << '"' << context->parent << "\" -> \"" << context << "\";\n";
-        }
-
-        auto str = context->getText();
-        std::replace( str.begin(), str.end(), '"', '\'');
-        file << '"' << context << '"' << "[label=\"" + name + str + "\"];\n";
+        linkWithParent(context, "basic");
+        return visitChildren(context);
+    }
+    antlrcpp::Any visitUnaryExpression(CParser::UnaryExpressionContext* context) override
+    {
+        linkWithParent(context, "unary");
+        return visitChildren(context);
+    }
+    antlrcpp::Any visitMultiplicativeExpression(CParser::MultiplicativeExpressionContext* context) override
+    {
+        linkWithParent(context, "multiplicative");
+        return visitChildren(context);
+    }
+    antlrcpp::Any visitAdditiveExpression(CParser::AdditiveExpressionContext* context) override
+    {
+        linkWithParent(context, "additive");
+        return visitChildren(context);
+    }
+    antlrcpp::Any visitRelationalExpression(CParser::RelationalExpressionContext* context) override
+    {
+        linkWithParent(context, "relational");
+        return visitChildren(context);
+    }
+    antlrcpp::Any visitEqualityExpression(CParser::EqualityExpressionContext* context) override
+    {
+        linkWithParent(context, "equality");
+        return visitChildren(context);
+    }
+    antlrcpp::Any visitAndExpression(CParser::AndExpressionContext* context) override
+    {
+        linkWithParent(context, "and");
+        return visitChildren(context);
+    }
+    antlrcpp::Any visitOrExpression(CParser::OrExpressionContext* context) override
+    {
+        linkWithParent(context, "or");
+        return visitChildren(context);
+    }
+    antlrcpp::Any visitExpression(CParser::ExpressionContext* context) override
+    {
+        linkWithParent(context, "expression");
+        return visitChildren(context);
+    }
+    antlrcpp::Any visitFile(CParser::FileContext* context) override
+    {
+        linkWithParent(context, "file");
+        return visitChildren(context);
     }
 
-    std::ofstream file;
+    antlrcpp::Any visitTerminal(antlr4::tree::TerminalNode* node)
+    {
+        linkWithParent(node, node->getText());
+        return defaultResult();
+    }
+
+    void linkWithParent(antlr4::tree::ParseTree* context, const std::string& name)
+    {
+        stream << '"' << context->parent << "\" -> \"" << context << "\";\n";
+        stream << '"' << context << "\"[label=\"" + name + "\"]";
+    }
+
+    std::ofstream stream;
     std::filesystem::path path;
 };
 
-int main(int argc, const char* argv[])
+void runForExample(const std::filesystem::path& path)
 {
-    std::ifstream stream("examples/json.json");
-
+    std::ifstream stream(path);
     antlr4::ANTLRInputStream input(stream);
-    JSONLexer lexer(&input);
+    CLexer lexer(&input);
     antlr4::CommonTokenStream tokens(&lexer);
-    JSONParser parser(&tokens);
+    CParser parser(&tokens);
 
-    auto* tree = parser.value();
+    const auto string = path.string();
+    const auto begin = string.find_first_of('/');
+    const auto end = string.find_last_of('.');
 
-    std::filesystem::create_directory("output");
-    DotVisitor visitor("output/test");
-    auto scene = visitor.visitValue(tree);
+    if(begin == std::string::npos or end == std::string::npos)
+        throw std::runtime_error("malformed path: " + string);
 
+    const std::filesystem::path output = "output";
+    std::filesystem::create_directory(output);
+
+    DotVisitor visitor(output / string.substr(begin + 1, end - begin - 1));
+    visitor.visitFile(parser.file());
+}
+
+void runForAllExamples(const std::filesystem::path& path = "examples")
+{
+    for(const auto& entry : std::filesystem::directory_iterator(path))
+    {
+        if(entry.is_directory())
+        {
+            runForAllExamples(entry.path());
+        }
+        else if(entry.is_regular_file())
+        {
+            runForExample(entry.path());
+        }
+        else
+        {
+            std::cerr << "unknown file type in examples: " + entry.path().string() << '\n';
+        }
+    }
+}
+
+
+int main(int argc, const char** argv)
+{
+    runForAllExamples();
     return 0;
 }
