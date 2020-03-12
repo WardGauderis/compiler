082f33ab4aeac92447b9ea87fb7de6b1bd7f5671
Ward <ward.gauderis@student.uantwerpen.be>
2020-03-12 17:42:13 +0100

    even better grammars

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,23 @@
 #include "CLexer.h"
 #include "CParser.h"
 
-
-std::filesystem::path swapTopFolder(const std::filesystem::path& path, const std::string& newName) {
+std::filesystem::path swapTopFolder(const std::filesystem::path& path, const std::string& newName)
+{
 	const auto string = path.string();
 	const auto begin = string.find_first_of('/');
 	const auto end = string.find_last_of('.');
 
-	if (begin == std::string::npos or end == std::string::npos)
-		throw std::runtime_error("malformed path: " + string);
+	if (begin==std::string::npos or end==std::string::npos)
+		throw std::runtime_error("malformed path: "+string);
 
-	return std::filesystem::path("output") / string.substr(begin + 1, end - begin - 1);
+	return std::filesystem::path("output")/string.substr(begin+1, end-begin-1);
 }
 
-void runTest(const std::filesystem::path& path, bool redoExisting) {
+void runTest(const std::filesystem::path& path, bool redoExisting)
+{
 	const auto output = swapTopFolder(path, "output");
-	if (redoExisting or not std::filesystem::exists(output.string() + ".png")) {
+	if (redoExisting or not std::filesystem::exists(output.string()+".png"))
+	{
 		std::ifstream stream(path);
 		antlr4::ANTLRInputStream input(stream);
 		CLexer lexer(&input);
@@ -31,20 +33,27 @@ void runTest(const std::filesystem::path& path, bool redoExisting) {
 	}
 }
 
-void runTests(const std::filesystem::path& path, bool redoExisting) {
-	for (const auto& entry : std::filesystem::directory_iterator(path)) {
-		if (entry.is_directory()) {
+void runTests(const std::filesystem::path& path, bool redoExisting)
+{
+	for (const auto& entry : std::filesystem::directory_iterator(path))
+	{
+		if (entry.is_directory())
+		{
 			runTests(entry.path(), redoExisting);
-		} else if (entry.is_regular_file()) {
+		}
+		else if (entry.is_regular_file())
+		{
 			runTest(entry.path(), redoExisting);
-		} else {
-			std::cerr << "unknown file type in examples: " + entry.path().string() << '\n';
+		}
+		else
+		{
+			std::cerr << "unknown file type in examples: "+entry.path().string() << '\n';
 		}
 	}
 }
 
-
-int main(int argc, const char** argv) {
+int main(int argc, const char** argv)
+{
 //	runTests("tests", true);
 
 //	std::stringstream stream("1+1+1;");
@@ -61,10 +70,10 @@ int main(int argc, const char** argv) {
 
 	const auto root = visitFile(node);
 	std::ofstream file("text.dot");
-    file << "digraph G\n";
-    file << "{\n";
+	file << "digraph G\n";
+	file << "{\n";
 	file << root;
-    file << "}\n" << std::flush;
-    system(("dot -Tpng " + std::string("text.dot") + " -o " + std::string("text.png")).c_str());
+	file << "}\n" << std::flush;
+	system(("dot -Tpng "+std::string("text.dot")+" -o "+std::string("text.png")).c_str());
 	return 0;
 }
