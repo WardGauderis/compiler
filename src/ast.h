12465e9ef77c78455d964b5e96d15406e42c0ed1
funkymaniac <thomas@dooms.eu>
2020-03-13 12:00:10 +0100

    beetje verder gewerkt

--- src/ast.h
+++ src/ast.h
@@ -6,8 +6,9 @@
 
 #include <array>
 #include <memory>
-#include <vector>
+#include <optional>
 #include <variant>
+#include <vector>
 
 namespace Ast
 {
@@ -21,23 +22,32 @@ struct Node
     [[nodiscard]] virtual std::string name() const = 0;
     [[nodiscard]] virtual std::string value() const = 0;
     [[nodiscard]] virtual std::vector<Node*> children() const = 0;
+    [[nodiscard]] virtual std::string color() const = 0;
 };
 
 struct Expr : public Node
 {
     explicit Expr() = default;
+    [[nodiscard]] std::string color() const final;
 };
 
-struct File final : public Node
+struct Statement : public Node
 {
-    explicit File(std::vector<Expr*> expressions)
+    Statement() = default;
+    [[nodiscard]] std::string color() const final;
+};
+
+struct Block final : public Node
+{
+    explicit Block(std::vector<Statement*> expressions)
     : expressions(std::move(expressions)) {}
 
-	std::vector<Expr*> expressions;
+	std::vector<Statement*> expressions;
 
     [[nodiscard]] std::string name() const final;
     [[nodiscard]] std::string value() const final;
     [[nodiscard]] std::vector<Node*> children() const final;
+    [[nodiscard]] std::string color() const final;
 };
 
 struct BinaryExpr final : public Expr
@@ -55,9 +65,9 @@ struct BinaryExpr final : public Expr
     [[nodiscard]] std::vector<Node*> children() const override;
 };
 
-struct PrefixExpr final : public Expr
+struct UnaryExpr final : public Expr
 {
-    explicit PrefixExpr(std::string operation, Expr* operand)
+    explicit UnaryExpr(std::string operation, Expr* operand)
     : operation(std::move(operation)), operand(operand) {}
 
 	std::string operation;
@@ -68,63 +78,88 @@ struct PrefixExpr final : public Expr
     [[nodiscard]] std::vector<Node*> children() const final;
 };
 
-struct Literal : public Expr
+struct Literal final : public Expr
 {
     template<typename Type>
     explicit Literal(Type val) : literal(val) {}
 
-    [[nodiscard]] std::string name() const final { return "literal"; }
-    [[nodiscard]] std::string value() const final { return ""; }
-    [[nodiscard]] std::vector<Node*> children() const final { return {}; }
+    [[nodiscard]] std::string name() const final;
+    [[nodiscard]] std::string value() const final;
+    [[nodiscard]] std::vector<Node*> children() const final;
 
     std::variant<char, short, int, long, float, double> literal;
 };
 
 struct Variable : public Expr
 {
-    Variable(std::string name) : name(std::move(name)) {}
+    explicit Variable(std::string name, bool isConst)
+    : identifier(std::move(name)), isConst(isConst) {}
 
-    std::string name;
+    [[nodiscard]] std::string name() const override;
+    [[nodiscard]] std::string value() const override;
+    [[nodiscard]] std::vector<Node*> children() const override;
+
+    std::string identifier;
+    bool isConst;
 };
 
+struct Pointer final : public Variable
+{
+    explicit Pointer(std::string name, bool isConst, Variable* pointee)
+    : Variable(std::move(name), isConst), pointee(pointee) {}
+
+    [[nodiscard]] std::string name() const final;
+    [[nodiscard]] std::string value() const final;
+    [[nodiscard]] std::vector<Node*> children() const final;
+
+    Variable* pointee;
+};
+
+struct Assignment final : public Statement
+{
+    explicit Assignment(std::string variable, Expr* expr)
+    : variable(std::move(variable)), expr(expr) {}
+
+    [[nodiscard]] std::string name() const final;
+    [[nodiscard]] std::string value() const final;
+    [[nodiscard]] std::vector<Node*> children() const final;
+
+    std::string variable;
+    Expr* expr;
+};
+
+struct Declaration final : public Statement
+{
+    explicit Declaration(Variable* assignee, Expr* expr)
+    : assignee(assignee), expr(expr) {}
+
+    [[nodiscard]] std::string name() const final;
+    [[nodiscard]] std::string value() const final;
+    [[nodiscard]] std::vector<Node*> children() const final;
+
+    Variable* assignee;
+    Expr* expr;  // can be nullptr
+};
+
+
+
 }
 
-////////////////////////
-
-namespace detail
-{
-    template <class... Fs>
-    struct overload;
-
-    template <class F>
-    struct overload<F> : public F
-    {
-        explicit overload(F f) : F(f) {}
-    };
-
-    template <class F, class... Fs>
-    struct overload<F, Fs...> : public overload<F>, overload<Fs...>
-    {
-        explicit overload(F f, Fs... fs) : overload<F>(f), overload<Fs...>(fs...) {}
-
-        using overload<F>::operator();
-        using overload<Fs...>::operator();
-    };
-}  // namespace detail
-
-template <class... F>
-auto overloaded(F... f)
-{
-    return detail::overload<F...>(f...);
-}
+// helpers for the ast traversal
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
 template<typename Func>
 void downcast_call(Ast::Node* node, const Func& func)
 {
-    if     (auto* res = dynamic_cast<Ast::File*      >(node)) func(res);
+    if     (auto* res = dynamic_cast<Ast::Block*     >(node)) func(res);
     else if(auto* res = dynamic_cast<Ast::BinaryExpr*>(node)) func(res);
-    else if(auto* res = dynamic_cast<Ast::PrefixExpr* >(node)) func(res);
+    else if(auto* res = dynamic_cast<Ast::UnaryExpr* >(node)) func(res);
     else if(auto* res = dynamic_cast<Ast::Literal*   >(node)) func(res);
+    else if(auto* res = dynamic_cast<Ast::Assignment*>(node)) func(res);
+    // we need to swap the order of pointer and variable, because dynamic cast
+    else if(auto* res = dynamic_cast<Ast::Pointer*   >(node)) func(res);
+    else if(auto* res = dynamic_cast<Ast::Variable*  >(node)) func(res);
 }
 
 
--- src/visitor.h
+++ src/visitor.h
@@ -34,9 +34,14 @@ namespace
             or is_type<CParser::MultiplicativeExprContext>(context);
     }
 
-    bool is_unary_expr(antlr4::tree::ParseTree* context)
+    bool is_prefix_expr(antlr4::tree::ParseTree* context)
     {
         return is_type<CParser::PrefixExprContext>(context);
+    }
+
+    bool is_postfix_expr(antlr4::tree::ParseTree* context)
+    {
+        return is_type<CParser::PostfixExprContext>(context);
     }
 
     bool is_expr(antlr4::tree::ParseTree* context)
@@ -52,6 +57,31 @@ namespace
     bool is_basic_expr(antlr4::tree::ParseTree* context)
     {
         return is_type<CParser::BasicExprContext>(context);
+    }
+
+    bool is_assign_expr(antlr4::tree::ParseTree* context)
+    {
+        return is_type<CParser::AssignExprContext>(context);
+    }
+
+    bool is_declaration(antlr4::tree::ParseTree* context)
+    {
+        return is_type<CParser::DeclarationContext>(context);
+    }
+
+    bool is_initializer(antlr4::tree::ParseTree* context)
+    {
+        return is_type<CParser::InitizalizerContext>(context);
+    }
+
+    bool is_typename(antlr4::tree::ParseTree* context)
+    {
+        return is_type<CParser::TypeNameContext>(context);
+    }
+
+    bool is_pointer_type(antlr4::tree::ParseTree* context)
+    {
+        return is_type<CParser::PointerTypeContext>(context);
     }
 }
 
@@ -76,7 +106,7 @@ Ast::Expr* visitExpr(antlr4::tree::ParseTree* context)
         }
         else throw std::logic_error("binary expr must have 1 or 3 children");
     }
-    else if(is_unary_expr(context))
+    else if(is_prefix_expr(context))
     {
         if(context->children.size() == 1)
         {
@@ -85,7 +115,7 @@ Ast::Expr* visitExpr(antlr4::tree::ParseTree* context)
         else if(context->children.size() == 2)
         {
             const auto operand = visitExpr(context->children[1]);
-            return new Ast::PrefixExpr(std::move(context->children[0]->getText()), operand);
+            return new Ast::UnaryExpr(std::move(context->children[0]->getText()), operand);
         }
         else throw std::logic_error("unary expr must have 1 or 2 children");
     }
@@ -105,18 +135,47 @@ Ast::Expr* visitExpr(antlr4::tree::ParseTree* context)
     else throw std::logic_error(std::string("unknown type: ") + typeid(*context).name());
 }
 
+Ast::Statement* visitStatement(antlr4::tree::ParseTree* context)
+{
+    if(is_assign_expr(context))
+    {
+        if(context->children.size() == 1)
+        {
+            visitExpr(context->children[0]);
+        }
+        else if(context->children.size() == 3)
+        {
+            const auto expr = visitExpr(context->children[2]);
+            return new Ast::Assignment(context->children[0]->getText(), expr);
+        }
+        else throw std::logic_error("assign expr must have 1 or 3 children");
+    }
+    else if(is_declaration(context))
+    {
+        if(context->children.size() == 3)
+        {
+            //TODO figure out good system for typenames and such
+        }
+        else if(context->children.size() == 5)
+        {
+
+        }
+        else throw std::logic_error("declaration must have 3 or 5 children");
+    }
+}
+
 std::unique_ptr<Ast::Node> visitFile(antlr4::tree::ParseTree* context)
 {
     if(is_file(context))
     {
         const auto size = context->children.size() - 1;
-        std::vector<Ast::Expr*> exprs(size);
+        std::vector<Ast::Statement*> exprs(size);
 
         for(size_t i = 0; i < size; i++)
         {
-            exprs[i] = visitExpr(context->children[i]);
+            exprs[i] = visitStatement(context->children[i]);
         }
-        auto result = std::make_unique<Ast::File>(exprs);
+        auto result = std::make_unique<Ast::Block>(exprs);
         foldFile(result);
         return result;
     }
