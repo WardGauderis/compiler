8617feda87c9bd4786e8f0326e4785fb3eec5d7e
funkymaniac <thomas@dooms.eu>
2020-04-05 14:36:51 +0200

    test

---
+++ tests/assignment/array.c
@@ -0,0 +1,14 @@
+// this is a test for the assign arrays feature
+
+int main()
+{
+    int a[10][5];
+    int b[5];
+    int** c = a;
+    c[5] = b;
+
+    for (int i = 0; i<5; ++i)
+    {
+        printf("%d\n", c[5][i]);
+    }
+}
